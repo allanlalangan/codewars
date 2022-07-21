f9fe50d39d034e4e4d1a1f6f99866cda9cfb1afb
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-21 10:53:14 -0700

    progress(5-kyu): luck-check update attempt
    !failed

--- 5-kyu/luck-check/index.js
+++ 5-kyu/luck-check/index.js
@@ -1,47 +1,42 @@
 const luckCheck = (str) => {
   let numbers;
-  // split string into numbers array
-  if (typeof str === 'string') {
-    // convert string digits to number
-    numbers = [...str.trim()].map((digit) => Number(digit));
-  }
-  // if input is number, separate digits as values in array
-  else {
+
+  if (typeof str === 'number') {
     numbers = Array.from(String(str), Number);
+  } else if (typeof str === 'string' && str.split(' ').join('').length >= 1) {
+    numbers = [...str.split(' ').join('')].map((digit) => Number(digit));
   }
 
-  // evaluate and throw error if any value isNaN
   if (
     (isNaN(str) && typeof str !== 'string') ||
-    str.length < 1 ||
+    numbers.length < 1 ||
     numbers.some((num) => isNaN(num))
   ) {
     throw new Error('Bad ticket');
   }
 
-  // init variables of numbers of each half of string
   let leftNumbers;
   let rightNumbers;
 
-  // split array down the middle and assign as left and right numbers
   if (numbers.length % 2 === 0) {
     leftNumbers = numbers.slice(0, numbers.length / 2);
     rightNumbers = numbers.slice(numbers.length / 2);
   } else {
-    // exclude middle number if odd amount of numbers, then split
     leftNumbers = numbers.slice(0, (numbers.length - 1) / 2);
     rightNumbers = numbers.slice((numbers.length + 1) / 2, numbers.length);
   }
-  // reduce numbers to find sum
+
   const leftSum = leftNumbers.reduce((a, b) => a + b, 0);
   const rightSum = rightNumbers.reduce((a, b) => a + b, 0);
-  // evaluate if sums are equal and return true / false
+
   const ticketIsLucky = leftSum === rightSum ? true : false;
   return ticketIsLucky;
 };
 
-// luckCheck('12345'); // false
-// luckCheck('121'); // true
-// luckCheck(' 1234231'); // true
+// luckCheck('12329'); // false
+// luckCheck('1 2 1'); // true
+// luckCheck('1234231'); // true
+// luckCheck(12291); // false
+// luckCheck(912291); // true
 // luckCheck('112a1'); // error
 // luckCheck(''); // error
