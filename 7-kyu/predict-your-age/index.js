a17a6a132c791a07a770aff33640b4103fa1c134
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-07-22 00:30:37 -0700

    solved(7-kyu): predict-your-age

--- 7-kyu/predict-your-age/index.js
+++ 7-kyu/predict-your-age/index.js
@@ -1,5 +1,13 @@
-const ages = [65, 60, 75, 55, 60, 63, 64, 45];
+const allAges = [65, 60, 75, 55, 60, 63, 64, 45];
 
-const predictAge = (ages) => {};
+const predictAge = (...ages) => {
+  const agesSquared = ages.map((age) => age * age);
+  const squaredSum = agesSquared.reduce((a, b) => a + b, 0);
+  const sumSqrt = Math.sqrt(squaredSum);
+  const finalResult = Math.floor(sumSqrt / 2);
 
-predictAge(ages);
+  console.log(finalResult);
+  return finalResult;
+};
+
+predictAge(...allAges);
