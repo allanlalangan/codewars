c79f0b8f4f78de318de525ee9e48b7079d1cd301
allanlalangan <allanlalangan@gmail.com>
2023-01-30 21:13:24 -0800

    solved(6-kyu): convert-string-to-camelcase

--- 6-kyu/convert-string-to-camelcase/index.js
+++ 6-kyu/convert-string-to-camelcase/index.js
@@ -1,6 +1,16 @@
 function toCamelCase(str) {
-  const splitString = str.split;
-  return str;
+	return str
+		.split('')
+		.map((char, i, arr) => {
+			if (char === '-' || char === '_') {
+				return '';
+			}
+			if (arr[i - 1] === '-' || arr[i - 1] === '_') {
+				return char.toUpperCase();
+			}
+			return char;
+		})
+		.join('');
 }
 
 console.log(toCamelCase('convert-this'));
